46db3d8b399f2c9eac6ddf1e261ebfdd6e9b3bfb
ritatechie <rita.nwaodu@gmail.com>
2022-06-07 02:43:12 -0700

    all errors fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
+
+from sys import argv, exit as sys_exit
+
 from calculator_1 import add, sub, mul, div
 
 
-def arg_calc(argv):
-     n = len(argv) - 1
-     if n != 3:
-         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-         exit(1)
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
-    if op == '+':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
-    elif op == '/':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-
 if __name__ == "__main__":
-    import sys
-    arg_calc(sys.argv)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
+    try:
+        a, op, b = argv[1:]
+
+    except ValueError:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys_exit(1)
+
+    try:
+        print(a, op, b, "=", ops[op](int(a), int(b)))
+
+    except KeyError:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys_exit(1)
+
+    except ValueError:
+        print("Invalid operand. Must be an integer")
+        sys_exit(1)
+
+    except ZeroDivisionError:
+        print("Invalid operation. Cannot divide by zero")
+        sys_exit(1)
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-         while i <= n:
-             print("{:d}: {:s}".format(i, argv[i]))
-             i += 1
+
+from sys import argv
 
 
 if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
+
+    nargs = len(argv) - 1
+
+    if nargs == 0:
+        print("0 arguments.")
+    elif nargs == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(nargs))
+
+    for i, v in enumerate(argv[1:]):
+        print("{}: {}".format(i + 1, v))
